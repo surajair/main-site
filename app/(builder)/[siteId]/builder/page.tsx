3cb03c3491aa335af8df69d12b88654e70f360db
Suraj Air <surajair@gmail.com>
2025-08-22 14:46:17 +0530

    refactor: remove unused block imports from page component

--- app/(builder)/[siteId]/builder/page.tsx
+++ app/(builder)/[siteId]/builder/page.tsx
@@ -2,13 +2,6 @@
 
 import ChaiBuilder from "chai-next";
 import "chai-next/builder-styles";
-import "../../../../blocks/accordion";
-import "../../../../blocks/aspect-ratio";
-import "../../../../blocks/avatar";
-import "../../../../blocks/badge";
-import "../../../../blocks/popover";
-import "../../../../blocks/text-reveal";
-import "../../../../blocks/typewriter-effect";
 
 export default function Page() {
   return <ChaiBuilder />;
