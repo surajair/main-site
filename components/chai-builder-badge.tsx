b89811062adbcd61cb06cbdbf00148f210be787a
Suraj Air <surajair@gmail.com>
2025-09-17 15:27:43 +0530

    Update chai-builder-badge.tsx

--- components/chai-builder-badge.tsx
+++ components/chai-builder-badge.tsx
@@ -15,7 +15,7 @@ const ChaiBuilderBadge = ({ position = "bottom-left" }: { position?: keyof typeo
           variant="default"
           className="flex items-center justify-center gap-1 px-3 py-2 text-sm font-medium bg-black hover:bg-black border-[1px] border-purple-400 text-white  rounded-full">
           <div className="w-5 h-5 mr-1 rounded flex items-center justify-center">
-            <Image src="/assets/chai-builder-logo.png" alt="Chai Builder" className="rounded" width={18} height={18} />
+            <Image src="/assets/chai-builder-logo.png" alt="Chai Builder" className="rounded" width={18} height={18} unoptimized/>
           </div>
           <span className="font-normal">Made with</span>
           <span className="font-bold">Chai Builder</span>
