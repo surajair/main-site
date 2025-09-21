f316f36da5fce9b6839cf890cce2f0412362724f
Suraj Air <surajair@gmail.com>
2025-09-21 18:54:23 +0530

    refactor: remove blog editor pages and loading component

--- app/(dashboard)/[websiteId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function ProjectPage({ params }: { params: Promise<{ websiteId: string }> }) {
-  const { websiteId } = await params;
-  redirect(websiteId ? `/${websiteId}/editor` : `/`);
-}
