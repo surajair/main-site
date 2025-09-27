5231c2fe5e62c171be8210bb90a408263c1b7b7f
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2025-09-27 17:12:45 +0530

    refactor: move QueryClient to dedicated provider and optimize feature flag loading

--- lib/openfeature/feature-flag-provider.tsx
+++ lib/openfeature/feature-flag-provider.tsx
@@ -1,27 +1,54 @@
 "use client";
 
+import QueryClientProviderWrapper from "@/components/providers/query-client-provider";
+import { getClientSettings, getSites } from "@/lib/getter";
 import { InMemoryProvider, OpenFeature, OpenFeatureProvider } from "@openfeature/react-sdk";
+import { useQueryClient } from "@tanstack/react-query";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
 
-export function FeatureFlagProvider({ children, featureFlags }: { children: React.ReactNode; featureFlags?: any }) {
+function FeatureFlagProviderComponent({ children, featureFlags }: { children: React.ReactNode; featureFlags?: any }) {
+  const [loading, setLoading] = useState(true);
   const [providerReady, setProviderReady] = useState(false);
+  const queryClient = useQueryClient();
+
+  queryClient.prefetchQuery({
+    staleTime: Infinity,
+    gcTime: Infinity,
+    queryKey: ["client-settings"],
+    queryFn: getClientSettings,
+  });
+  queryClient.prefetchQuery({ staleTime: Infinity, gcTime: Infinity, queryKey: ["websites-list"], queryFn: getSites });
 
   useEffect(() => {
     try {
       const provider = new InMemoryProvider(featureFlags);
       OpenFeature.setProvider(provider);
       setProviderReady(true);
+      setTimeout(() => setLoading(false), 1000);
     } catch {}
   }, [featureFlags]);
 
-  if (!providerReady) {
-    return (
-      <div className="w-screen h-screen flex items-center justify-center">
-        <Loader className="text-primary animate-spin w-6 h-6" />
-      </div>
-    );
-  }
-
-  return <OpenFeatureProvider>{children}</OpenFeatureProvider>;
+  return (
+    <>
+      {loading && (
+        <div className="w-screen h-screen flex items-center justify-center">
+          <Loader className="text-primary animate-spin w-6 h-6" />
+        </div>
+      )}
+      {providerReady && (
+        <div className={loading ? "sr-only" : ""}>
+          <OpenFeatureProvider>{children}</OpenFeatureProvider>
+        </div>
+      )}
+    </>
+  );
 }
+
+export const FeatureFlagProvider = ({ children, featureFlags }: { children: React.ReactNode; featureFlags?: any }) => {
+  return (
+    <QueryClientProviderWrapper>
+      <FeatureFlagProviderComponent featureFlags={featureFlags}>{children}</FeatureFlagProviderComponent>
+    </QueryClientProviderWrapper>
+  );
+};
