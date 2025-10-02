1530143bbe71a856cb598adc8620e45337d0e3cc
Suraj Air <surajair@gmail.com>
2025-10-02 22:43:24 +0530

    feat: add user authentication and websocket support to editor component

--- app/(editor)/[websiteId]/editor/editor.tsx
+++ app/(editor)/[websiteId]/editor/editor.tsx
@@ -7,14 +7,14 @@ import { registerFonts } from "@/fonts";
 import { registerPanels } from "@/utils/register-panels";
 import ChaiBuilder from "chai-next";
 import { startsWith } from "lodash";
-import { useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
-const getPreviewUrl = (slug: string, domain?: string) => {
-  return `//${domain}/api/preview?slug=${startsWith(slug, "/") ? slug : "/_partial/" + slug}`;
-};
-
-const getLiveUrl = (slug: string, domain?: string) => {
-  return `//${domain}/api/preview?disable=true&slug=${startsWith(slug, "/") ? slug : "/_partial/" + slug}`;
+type LoggedInUser = {
+  id: string;
+  email: string;
+  name: string;
+  avatar?: string;
+  metadata?: Record<string, any>;
 };
 
 registerPanels();
@@ -22,21 +22,57 @@ registerBlocks();
 registerFonts();
 
 export default function Editor({ domain, websiteId }: { domain?: string; websiteId?: string }) {
+  const [user, setUser] = useState<LoggedInUser | null>(null);
   const websiteSettings = useMemo(() => <WebsiteSettings websiteId={websiteId} />, [websiteId]);
+
+  useEffect(() => {
+    const checkInitialSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      if (session?.user) {
+        setUser({
+          id: session.user.id,
+          email: session.user.email,
+          name: session.user.user_metadata.name,
+        } as LoggedInUser);
+      }
+    };
+    checkInitialSession();
+  }, []);
+
+  const getAccessToken = useCallback(async () => {
+    const { data } = await supabase.auth.getSession();
+    return data?.session?.access_token ?? "";
+  }, []);
+
+  const getPreviewUrl = useCallback(
+    (slug: string) => `//${domain}/api/preview?slug=${startsWith(slug, "/") ? slug : "/_partial/" + slug}`,
+    [domain],
+  );
+
+  const getLiveUrl = useCallback(
+    (slug: string) => `//${domain}/api/preview?disable=true&slug=${startsWith(slug, "/") ? slug : "/_partial/" + slug}`,
+    [domain],
+  );
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <ChaiBuilder
+      currentUser={user}
       autoSave
       autoSaveInterval={20}
-      getAccessToken={async () => {
-        const { data } = await supabase.auth.getSession();
-        return data?.session?.access_token ?? "";
-      }}
+      getAccessToken={getAccessToken}
       hasReactQueryProvider
       apiUrl="editor/api"
       // @ts-ignore
       topLeftCorner={() => websiteSettings}
-      getPreviewUrl={(slug: string) => getPreviewUrl(slug, domain)}
-      getLiveUrl={(slug: string) => getLiveUrl(slug, domain)}
+      getPreviewUrl={getPreviewUrl}
+      getLiveUrl={getLiveUrl}
+      websocket={supabase}
     />
   );
 }
