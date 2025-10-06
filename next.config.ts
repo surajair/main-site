8a78a630da64dd18111d360064e9093e042d2b5b
Suraj Air <surajair@gmail.com>
2025-10-06 18:20:19 +0530

    chore: enable turbopack by default and update sharp dependency override

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import { withBotId } from "botid/next/config";
 import withChaiBuilder from "chai-next/config";
-import { IMAGE_DOMAINS } from "./remote-pattern";
 import type { NextConfig } from "next";
+import { IMAGE_DOMAINS } from "./remote-pattern";
 
 const nextConfig: NextConfig = {
   images: {
     //NOTE: Update this list as needed
     remotePatterns: [...IMAGE_DOMAINS.map((domain) => ({ hostname: domain }))],
   },
-  serverExternalPackages: ["sharp"],
 };
 export default withBotId(withChaiBuilder(nextConfig));
