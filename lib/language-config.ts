671850e84e210c604b88b9a8aa22748e9b220b81
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-10-06 15:28:10 +0530

    feat: add Slovenian language support to base language codes

--- lib/language-config.ts
+++ lib/language-config.ts
@@ -1,7 +1,8 @@
 // Default language codes with only English as base
 const BASE_LANGUAGE_CODES = {
   en: "English",
-  fr: "French"
+  fr: "French",
+  sl: "Slovenian",
 };
 const envLanguages = process.env.NEXT_PUBLIC_SUPPORTED_LANGUAGES;
 
