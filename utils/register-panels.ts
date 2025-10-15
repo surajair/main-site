8374ef96ae1d7b1cdb048b68fde2cab638534952
Suraj Air <surajair@gmail.com>
2025-10-15 17:42:49 +0530

    refactor: remove genai panel and block tab registration

--- utils/register-panels.ts
+++ utils/register-panels.ts
@@ -2,8 +2,6 @@ import { formsPanel } from "@/components/website-settings/form-panel";
 import { helpPanel } from "@/components/website-settings/help-panel";
 import { profilePanel } from "@/components/website-settings/profile-panel";
 import { upgradePanel } from "@/components/website-settings/upgrade-panel";
-import { withGenAIPanel } from "@/components/website-settings/with-genai-panel";
-import { registerChaiAddBlockTab } from "@chaibuilder/sdk";
 import { registerChaiSidebarPanel } from "chai-next";
 
 export const registerPanels = () => {
@@ -11,7 +9,4 @@ export const registerPanels = () => {
   registerChaiSidebarPanel("help-panel", helpPanel);
   registerChaiSidebarPanel("forms-panel", formsPanel);
   registerChaiSidebarPanel("upgrade-modal", upgradePanel);
-
-  // Register Chai Add Block Tab
-  registerChaiAddBlockTab("with-genai", withGenAIPanel);
 };
