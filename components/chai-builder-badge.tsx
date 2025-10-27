9405527ede6f1ac3e021c177c37bf0ef26997511
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-10-27 20:21:31 +0530

    feat: add support for custom made-with badge through client settings

--- components/chai-builder-badge.tsx
+++ components/chai-builder-badge.tsx
@@ -7,20 +7,42 @@ const positionOptions = {
   "bottom-right": "bottom-6 right-6",
 } as const;
 
-const ChaiBuilderBadge = ({ position = "bottom-left" }: { position?: keyof typeof positionOptions }) => {
+const ChaiBuilderBadge = ({
+  position = "bottom-left",
+  madeWithBadge,
+}: {
+  position?: keyof typeof positionOptions;
+  madeWithBadge?: string;
+}) => {
   return (
     <div className={`fixed ${positionOptions[position]} z-50`}>
-      <Link href={"https://www.chaibuilder.com"} target="_blank" className="cursor-pointer">
-        <Badge
-          variant="default"
-          className="flex items-center justify-center gap-1 px-3 py-2 text-sm font-medium bg-black hover:bg-black border-[1px] border-purple-400 text-white  rounded-full">
-          <div className="w-5 h-5 mr-1 rounded flex items-center justify-center">
-            <Image src="/assets/chai-builder-logo.webp" alt="Chai Builder" className="rounded" width={18} height={18} unoptimized/>
-          </div>
-          <span className="font-normal">Made with</span>
-          <span className="font-bold">Chai Builder</span>
-        </Badge>
-      </Link>
+      <Badge
+        variant="default"
+        className="p-2 bg-black hover:bg-black border-[1px] border-purple-400 text-white  rounded-full">
+        {madeWithBadge ? (
+          <div dangerouslySetInnerHTML={{ __html: madeWithBadge }} />
+        ) : (
+          <>
+            <Link
+              href={"https://www.chaibuilder.com"}
+              target="_blank"
+              className="cursor-pointer flex items-end justify-center gap-1 text-sm font-medium">
+              <div className="w-5 h-5 mr-1 rounded flex items-center justify-center">
+                <Image
+                  src="/assets/chai-builder-logo.webp"
+                  alt="Chai Builder"
+                  className="rounded"
+                  width={18}
+                  height={18}
+                  unoptimized
+                />
+              </div>
+              <span className="font-normal">Made with</span>
+              <span className="font-bold">Chai Builder</span>
+            </Link>
+          </>
+        )}
+      </Badge>
     </div>
   );
 };
